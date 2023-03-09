6259fd5a2f5cf6cf3bc51e3e030da61fe69e5eb6
David Carlyn <davidecarlyn@gmail.com>
2023-03-09 11:23:31 -0500

    Diffusion model created. Not sure if training was been optimized or it just takes a long time to train.

--- main.py
+++ main.py
@@ -6,27 +6,98 @@ from torchvision.transforms import ToTensor, ToPILImage, Resize
 
 from PIL import Image
 
+import matplotlib.pyplot as plt
+
+def embed_timestep(x, t):
+    ch = x.shape[1]
+    ch_h = ch // 2
+    if ch_h <= 1:
+        emb = torch.log(torch.tensor(10000)).cuda()
+    else:
+        emb = torch.log(torch.tensor(10000)).cuda()/(ch_h-1)
+    emb = torch.exp(torch.arange(0, ch_h).cuda()*-emb)
+    emb = torch.tensor(t).unsqueeze(0).cuda() * emb.unsqueeze(0)
+    emb = torch.cat((torch.sin(emb), torch.cos(emb)), dim=1)
+    
+    if ch % 2 == 1:
+        emb = torch.cat((emb, torch.zeros_like(emb)[:, :1]), dim=1)
+    
+    emb = emb.unsqueeze(2).unsqueeze(3).repeat((1, 1, x.shape[2], x.shape[3]))
+    return x + emb
+
 class DDPM(nn.Module):
     def __init__(self, beta_start, beta_end, T) -> None:
         super().__init__()
-        self.betas = torch.linspace(0.0001, 0.02, args.T)
+        self.betas = torch.linspace(beta_start, beta_end, args.T).cuda()
         self.alphas = 1 - self.betas
 
-        self.recon_layers = []
-        for _ in range(T):
-            self.recon_layers.append(nn.Linear(4096, 4096))
+        self.encode = nn.Sequential(
+            nn.Conv2d(3, 16, 3, 1, 1),
+            nn.ReLU(),
+            nn.Conv2d(16, 16, 3, 1, 1),
+            nn.ReLU(),
+            nn.Conv2d(16, 16, 3, 1, 1),
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2, padding=0),
+            nn.Conv2d(16, 32, 3, 1, 1),
+            nn.ReLU(),
+            nn.Conv2d(32, 32, 3, 1, 1),
+            nn.ReLU(),
+            nn.Conv2d(32, 32, 3, 1, 1),
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2, padding=0),
+            nn.Conv2d(32, 64, 3, 1, 1),
+            nn.ReLU(),
+            nn.Conv2d(64, 64, 3, 1, 1),
+            nn.ReLU(),
+            nn.Conv2d(64, 64, 3, 1, 1),
+            nn.ReLU(),
+        )
 
+        self.decode = nn.Sequential(
+            nn.Conv2d(64, 32, 3, 1, 1),
+            nn.ReLU(),
+            nn.Conv2d(32, 32, 3, 1, 1),
+            nn.ReLU(),
+            nn.Conv2d(32, 32, 3, 1, 1),
+            nn.ReLU(),
+            nn.ConvTranspose2d(32, 32, 2, 2, padding=0),
+            nn.ReLU(),
+            nn.Conv2d(32, 16, 3, 1, 1),
+            nn.ReLU(),
+            nn.Conv2d(16, 16, 3, 1, 1),
+            nn.ReLU(),
+            nn.Conv2d(16, 16, 3, 1, 1),
+            nn.ReLU(),
+            nn.ConvTranspose2d(16, 16, 2, 2, padding=0),
+            nn.ReLU(),
+            nn.Conv2d(16, 3, 3, 1, 1),
+            nn.ReLU(),
+            nn.Conv2d(3, 3, 3, 1, 1),
+            nn.ReLU(),
+            nn.Conv2d(3, 3, 3, 1, 1),
+            nn.Tanh()
+        )
+
+        self.linear_encode = nn.Conv2d(3, 3, 1, 1, 0)
+        self.linear_decode = nn.Conv2d(64, 64, 1, 1, 0)
+        
     def diffuse(self, x_0, t):
-        a_comp = self.alphas[:t].prod()
+        a_comp = self.alphas[:t+1].prod()
 
-        self.epsilon = torch.normal(0, 1, size=x_0.shape)
+        self.epsilon = torch.normal(0, 1, size=x_0.shape).cuda()
 
         x_t = torch.sqrt(a_comp) * x_0 + (1-a_comp) * self.epsilon
 
         return x_t
 
     def denoise(self, x_t, t):
-        return self.recon_layers[t](x_t)
+        z = self.encode(embed_timestep(x_t, t))
+        out = self.decode(embed_timestep(z, t)) 
+        #z = self.encode(self.linear_encode(embed_timestep(x_t, t)))
+        #out = self.decode(self.linear_decode(embed_timestep(z, t))) 
+        #out = (out + 1)/2
+        return out
 
     def forward(self, x_0, t):
         x_t = self.diffuse(x_0, t)
@@ -38,11 +109,16 @@ def save_img(x_t, path):
     out = ToPILImage()(out)
     out.save(path)
 
+def plot(losses):
+    plt.plot(losses)
+    plt.savefig("loss_plot.png")
+    plt.close()
+
 if __name__ == "__main__":
     parser = ArgumentParser()
     parser.add_argument("--T", type=int, default=1000)
-    parser.add_argument("--t", type=int, default=1000)
-    parser.add_argument("--lr", type=float, default=0.001)
+    parser.add_argument("--epochs", type=int, default=1000)
+    parser.add_argument("--lr", type=float, default=0.0001)
     args = parser.parse_args()
 
 
@@ -55,16 +131,36 @@ if __name__ == "__main__":
     loss_fn = nn.MSELoss()
     ddpm = DDPM(0.0001, 0.02, args.T).cuda()
     optimizer = torch.optim.Adam(ddpm.parameters(), lr=args.lr)
-    for epoch in range(1000):
-        t = torch.randint(0, args.T)
-        out = ddpm(x, t)
-        loss = loss_fn(ddpm.epsilon, out)
+    losses = []
+    for epoch in range(args.epochs):
+        total_loss = 0
+        for k in range(args.T):
+            t = torch.randint(0, args.T, size=(1,)).cuda()
+            out = ddpm(x, t)
+            loss = loss_fn(ddpm.epsilon, out)# * (t+1)/args.T
 
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
 
-        print(f"Epoch: {epoch+1} | Loss: {loss.item()}")
+            total_loss += loss.item()
+        
+        print(f"Epoch: {epoch+1} | Loss: {total_loss}")
+        losses.append(total_loss)
+        plot(losses)
+
+        with torch.no_grad():
+            x_T = torch.normal(0, 1, size=x.shape).cuda()
+            for t in range(args.T-1, -1, -1):
+                alpha = ddpm.alphas[t]
+                noise_pred = ddpm.denoise(x_T, t)
+                z = torch.normal(0, 1, size=x_T.shape).cuda()
+                if t == 0:
+                    z = torch.zeros_like(x_T)
+                x_T = (1/torch.sqrt(alpha)) * (x_T - (1 - alpha)/torch.sqrt(1-ddpm.alphas[:t+1].prod()) * noise_pred) + torch.sqrt(ddpm.betas[t])*z
+            save_img((x_T[0]+1)/2, "recon.png")
+            torch.save(ddpm.state_dict(), "model.pt")
+
 
 
 
